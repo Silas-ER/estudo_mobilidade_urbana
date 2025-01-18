eb825415a2f5d13a0c78563241cf6b638f63b770
Silas Eduardo <silasn1@live.com>
2025-01-18 16:21:47 -0300

    feat: criacao da classe visualizer

--- modules/insights.py
+++ modules/insights.py
@@ -1,8 +1,12 @@
+
 import streamlit as st
-from modules.visualizer import DataVisualizer
+from modules import DataVisualizer
 
 def insights(df):
-    # Análise Temporal
+
+    DataVisualizer(df)
+
+    # Analise Temporal
     st.markdown('### Análise temporal de uso')
     st.markdown("""
                 <p>
@@ -10,13 +14,11 @@ def insights(df):
                     agrupando pela coluna 'Mes_Ano' criada durante a limpeza e raspagem de dados.
                     <br>
                     Com isso, optamos por fazer um gráfico de linhas que expressaria da melhor forma a variação das viagens durante o tempo.
-                    <br>
                 </p>
                 """, unsafe_allow_html=True)
-
     col1, col2 = st.columns([1.1, 0.9])
-    with col1: plot_line_chart()
-    st.divider()
+    with col1:
+        st.pyplot(DataVisualizer.plot_line_chart)
 
     # Análise Comparativa
     st.markdown('### Análise dos tipos de bilhetagem')
@@ -26,14 +28,11 @@ def insights(df):
                     afim de descobrir os tipos de clientes das linhas.
                     <br>
                     Para tal optamos por um gráfico de barras comparando os tipos de bilhetagem armazenadas no dataframe.
-                    <br>
                 </p>
                 """, unsafe_allow_html=True)
-
     col1, col2 = st.columns([1.1, 0.9])
-    with col1: plot_bilhetagem_comparativa(df_bilhetagem_geral)
-    st.divider()
-
+    with col1:
+        plot_bilhetagem_comparativa(df_bilhetagem_geral)
     st.markdown('### Análise de volumes de viagem por linha')
     st.markdown("""
                 <p>
@@ -42,23 +41,19 @@ def insights(df):
                     Filtramos também ordenando pelas top 10 linhas com maior quantidade de viagens, presumindo que essas são as com maiores demandas perante os usuários!
                     <br>
                     Com isso, optamos por fazer um gráfico de barras, mostrando os maiores quantidade de viagens por linha.
-                    <br>
                 </p>
                 """, unsafe_allow_html=True)
-
     col1, col2 = st.columns([1.1, 0.9])
-    with col1: plot_top_linhas(df_bilhetagem_geral)
-    st.divider()
-
+    with col1:
+        plot_top_linhas(df_bilhetagem_geral)
     st.markdown('### Análise de volume de viagens por empresa')
     st.markdown("""
                 <p>
                     Nesse caso optamos por fazer a análise de dados focada na quantidade de viagens por empresa apurando assim qual empresa teria maior impacto na mobilidade urbana.
                     <br>
                     Mantemos a ideia de um gráfico de barras que expressaria da melhor forma a variação das viagens por empresa.
-                    <br>
                 </p>
                 """, unsafe_allow_html=True)
-
     col1, col2 = st.columns([1.1, 0.9])
-    with col1: plot_top_empresas(df_bilhetagem_geral)
+    with col1:
+        plot_top_empresas(df_bilhetagem_geral)
--- modules/visualizer.py
+++ modules/visualizer.py
@@ -1,6 +1,7 @@
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 class DataVisualizer:
     def __init__(self, df):
@@ -26,3 +27,74 @@ class DataVisualizer:
         plt.ylabel("Densidade", fontsize=8)
 
         return plt.gcf()
+    
+    def plot_line_chart(self):
+
+        self.df['Mes_Ano'] = pd.to_datetime(self.df['Mes_Ano'])
+        df_temp = self.df.groupby('Mes_Ano')['Qtd_Viagens'].sum().reset_index()
+        
+        plt.figure(figsize=(12, 8))
+        sns.lineplot(data=df_temp, x='Mes_Ano', y='Qtd_Viagens')
+        plt.title('Evolução do Uso de Transporte Público em Natal/RN')
+        plt.xlabel('Tempo (mes/ano)')
+        plt.ylabel('Quantidade de Viagens')
+        #plt.xticks(rotation=90)
+        
+        return plt
+
+    def plot_bilhetagem_comparativa(self):
+        # Somar as colunas de tipos de bilhetagem
+        bilhetagem_cols = [
+            'Gratuito_Cartao', 'Gratuito_BT', 'Estudante_Cartao', 'Estudante_BT',
+            'Vale_Transporte', 'Inteira_Cartao', 'Inteira_Especie', 'Tarifa_Social'
+        ]
+
+        df_bilhetagem = self.df[bilhetagem_cols].sum().reset_index()
+        df_bilhetagem.columns = ['Tipo_Bilhetagem', 'Total_Viagens']
+        
+        # Ordenar os dados para visualização
+        df_bilhetagem = df_bilhetagem.sort_values(by='Total_Viagens', ascending=False)
+        
+        # Plotar gráfico de barras
+        plt.figure(figsize=(10, 6))
+        ax = sns.barplot(data=df_bilhetagem, x='Total_Viagens', y='Tipo_Bilhetagem', palette='viridis')
+        plt.title('Comparação dos Tipos de Bilhetagem')
+        plt.xlabel('Total de Viagens')
+        plt.ylabel('Tipo de Bilhetagem')
+        
+        # Formatador para mostrar valores reais no eixo X
+        formatter = ticker.FuncFormatter(lambda x, _: f'{int(x):,}'.replace(',', '.'))
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(12000000))  
+        ax.xaxis.set_major_formatter(formatter)
+
+        # Renderizar o gráfico no Streamlit
+        return plt
+    
+    def plot_top_linhas(self):
+        top_linhas = self.df.groupby('Linha')['Qtd_Viagens'].sum().reset_index()
+        top_linhas = top_linhas.sort_values(by='Qtd_Viagens', ascending=False).head(10)
+
+        plt.figure(figsize=(10, 6))
+        sns.barplot(data=top_linhas, x='Linha', y='Qtd_Viagens', palette='viridis')
+        plt.title('Top 10 Linhas com Maior Volume de Viagens')
+        plt.xlabel('Total de Viagens')
+        plt.ylabel('Linhas')
+
+        return plt
+
+    def plot_top_empresas(self):
+        top_empresas = self.df.groupby('Empresa')['Qtd_Viagens'].sum().reset_index()
+        top_empresas = top_empresas.sort_values(by='Qtd_Viagens', ascending=False).head(10)
+
+        plt.figure(figsize=(10, 6))
+        ax = sns.barplot(data=top_empresas, x='Qtd_Viagens', y='Empresa', palette='viridis')
+        plt.title('Top 10 Empresas com Maior Volume de Viagens')
+        plt.xlabel('Total de Viagens')
+        plt.ylabel('Empresas')
+
+        # Formatador para mostrar valores reais no eixo X
+        formatter = ticker.FuncFormatter(lambda x, _: f'{int(x):,}'.replace(',', '.'))
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(500000))  
+        ax.xaxis.set_major_formatter(formatter)
+
+        return plt
